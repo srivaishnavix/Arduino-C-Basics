5af46eb00c90ebbe33d29e4c79651b812653823c
srivaishnavix <vinnybhamidipati@gmail.com>
2023-12-28 23:54:45 +0530

    Simple star pattern code added

---
+++ 10_Loop_Structures/for_pattern/for_pattern.ino
@@ -0,0 +1,20 @@
+//printing pattern
+//*****
+//*****
+//*****
+//*****
+//*****
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for (int i = 0; i<5;i++){
+    Serial.println("*****");
+  }
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
