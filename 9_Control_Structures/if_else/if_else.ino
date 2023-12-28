512ea02e87bf2ca8a7bb26348988ce0a829f0e64
srivaishnavix <vinnybhamidipati@gmail.com>
2023-12-28 22:47:05 +0530

    added new file in control structure

---
+++ 9_Control_Structures/if_else/if_else.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  if (true){
+    Serial.println("My if statement");
+  }
+  else {
+  Serial.println("My else statement");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
